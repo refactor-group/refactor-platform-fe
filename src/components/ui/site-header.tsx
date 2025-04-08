d5faa251aada7d7040d3af275db85b8c1700199e
Jim Hodapp <james.hodapp@gmail.com>
2025-04-07 21:12:31 -0500

    Add some padding to the coaching session page title left side and from the main-nav header

--- src/components/ui/site-header.tsx
+++ src/components/ui/site-header.tsx
@@ -7,7 +7,7 @@ import { UserNav } from "@/components/ui/user-nav";
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full max-w-screen-2xl border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-14 pl-4 pt-2 max-w-screen-2xl items-start">
         <MainNav />
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
