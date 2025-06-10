bba532bfa680088595f88438b9aea7035228c4c0
Caleb Bourg <calebbourg2@gmail.com>
2025-06-10 08:13:15 -0400

    add concept of user roles

--- src/types/general.ts
+++ src/types/general.ts
@@ -111,6 +111,11 @@ export enum ItemStatus {
   WontDo = "WontDo",
 }
 
+export enum Role {
+  User = "User",
+  Admin = "Admin"
+}
+
 export function stringToActionStatus(statusString: string): ItemStatus {
   const status = statusString.trim();
 
